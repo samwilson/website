ec6deb9130b2b30f9dd8a856df7e19a2b8d3b232
Sam Wilson <sam@samwilson.id.au>
2016-03-30 09:06:54 +0800

    Install newer version of Pandoc.

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+## Install latest Pandoc.
+wget https://github.com/jgm/pandoc/releases/download/1.17.0.2/pandoc-1.17.0.2-1-amd64.deb
+sudo dpkg -i pandoc-1.17.0.2-1-amd64.deb
+
+## Switch to the master branch, build, and commit the updated HTML and PDF files.
 git checkout master
 build.sh
 git config user.name "Travis CI"
 git config user.email "sam@samwilson.id.au"
 git add -A
 git commit -m"Files built by Travis."
+
+## Push the changes back to Github.
 git push "https://${GH_TOKEN}@github.com/samwilson/samwilson.github.io.git" master:master
