6f794b23d529b83e4ad132b3a0e781f47c2f36c6
Sam Wilson <sam@samwilson.id.au>
2016-03-30 09:23:25 +0800

    Don't keep the pandoc install file.

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,13 +3,14 @@
 ## Install latest Pandoc.
 wget https://github.com/jgm/pandoc/releases/download/1.17.0.2/pandoc-1.17.0.2-1-amd64.deb
 sudo dpkg -i pandoc-1.17.0.2-1-amd64.deb
+rm pandoc-1.17.0.2-1-amd64.deb
 
 ## Switch to the master branch, build, and commit the updated HTML and PDF files.
 git checkout master
 build.sh
 git config user.name "Travis CI"
 git config user.email "sam@samwilson.id.au"
-git add -A
+git add -A \*.html \*.pdf
 git commit -m"Files built by Travis."
 
 ## Push the changes back to Github.
