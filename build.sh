3155b80911fd213aa5255b05a6deb9c3d98710e2
Sam Wilson <sam@samwilson.id.au>
2017-09-03 19:03:32 +0800

    Going manual

---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+## Set up the source and destination directories.
+THISDIR=$(cd $(dirname $0); pwd)
+cd "$THISDIR"
+OUTDIR=$THISDIR"/../master"
+if [ ! -e "$OUTDIR" ]; then mkdir "$OUTDIR"; fi
+
+## Set paths to the template files.
+HTMLTPL=$THISDIR"/html.tpl"
+PDFTPL=$THISDIR"/pdf.tpl"
+
+## Build all single-page files (HTML and PDF).
+ALL_MD_FILES=$(find $THISDIR -iname '*.md' -type f)
+for F in $ALL_MD_FILES; do
+
+	## Ignore the readme file.
+	if [ $(basename $F) = "README.md" ]; then continue; fi
+
+	## Extract just the 'local' part of the path.
+	BASE_LEN=${#THISDIR}+1
+	FILE=${F:$BASE_LEN:-3}
+	echo "Processing $FILE";
+	SLASHS=${FILE//[^\/]/}
+	SLASHCOUNT=${#SLASHS}
+	RELPATH=$(for X in $(seq $SLASHCOUNT); do echo -n "../"; done)
+
+	## Create required directories.
+	mkdir -p "$OUTDIR/"$(dirname "$FILE")
+
+	## Generate HTML version.
+	pandoc --standalone \
+		--css=/style.css \
+		--section-divs \
+		--template=$HTMLTPL \
+		--variable="filename:$FILE" \
+		--variable="relpath:$RELPATH" \
+		-f markdown \
+		-t html5 \
+		-o "$OUTDIR/$FILE.html" \
+		"$FILE.md"
+
+	## Generate PDF version.
+	pandoc --standalone \
+		--template=$PDFTPL \
+		--variable="filename:$FILE" \
+		-f markdown \
+		-o "$OUTDIR/$FILE.pdf" \
+		"$FILE.md"
+
+done
+
+## Copy images for HTML (they're already included in the PDFs).
+for F in $(find $THISDIR -iname '*.jpg' -type f); do
+	echo "Copying $F"
+	cp "$F" "$OUTDIR/."
+done
+
+## Copy stylesheet, images, and static HTML pages.
+for F in "style.css" "404.html" "index.html"; do
+	if [ -f "$THISDIR/$F" ]; then
+		cp $THISDIR"/"$F $OUTDIR/.
+	fi
+done
